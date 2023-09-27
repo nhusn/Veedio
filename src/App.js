26f31af58eee4380a49959862fe3d7b8695770c1
nhusn <mnhnafilhusn09@gmail.com>
2023-09-27 15:27:44 +0530

    media player first day

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Home from './Pages/Home';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Media player</h1>
+      <LandingPage/>
+      <Home/>
     </div>
   );
 }
